cc2a9acac3fe38756a1a4075b6121032ef2c9fa0
U-AD\alzakariyamq <alzakariyamq@msoe.edu>
2019-03-08 18:03:46 -0600

    implement simple debug feature to test all functions

--- asm_new/main.s
+++ asm_new/main.s
@@ -33,15 +33,25 @@ creative_main:
     push {lr}
     mov r7, r10
     ldr r5, [r7, #oToolkit_JoypadPtr]
-    ldrh r0, [r5, #oJoypad_Held]
+    ldrh r0, [r5, #oJoypad_Pressed]
     ldr r1, =#JOYPAD_SELECT
-    mul r0, r1
+    tst r0, r1
     bne .key_held
     b .return
 .key_held:
-    ldr r0, =TextScriptShukoCrossTut
-    mov r1, #3
-    blx chatbox_runScript, r2
+    // ldr r0, =TextScriptShukoCrossTut
+    // mov r1, #3
+    // blx chatbox_runScript, r3
+.test_function
+    ldr r6, =byte_200CDBC // looks like some free 100 bytes
+    ldr r4, [r6, #0]
+    ldr r0, [r6, #4]
+    ldr r1, [r6, #8]
+    ldr r2, [r6, #0xC]
+    ldr r3, [r6, #0x10]
+    ldr r5, [r6, #0x14]
+    mov lr, pc
+    bx r4
 .return:
     pop {pc}
     .pool
